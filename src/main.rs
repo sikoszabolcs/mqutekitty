33b0623b336483653d7e1ef750876a0453999ca1
Szabolcs Sikó <szabolcs.siko@siemens.com>
2022-11-02 15:15:04 +0000

    Simplify moves & borrows in the connect function

--- src/main.rs
+++ src/main.rs
@@ -13,48 +13,37 @@ pub mod conn_ack_packet;
 pub mod connect_packet;
 pub mod control_packets;
 
-pub struct MyQuteKittyClient<'a> {
+pub struct MyQuteKittyClient {
     connect_flags: u8,
-    server_address: Option<&'a str>,
+    server_address: String,
     tcp_stream: Option<TcpStream>,
 }
 
-impl<'a> MyQuteKittyClient<'a> {
+impl MyQuteKittyClient {
     pub fn new(connect_flags: u8) -> Self {
         return MyQuteKittyClient {
             connect_flags,
-            server_address: None,
+            server_address: "".to_string(),
             tcp_stream: None,
         };
     }
 
-    pub fn connect(&'a mut self, address: &'a str) -> Result<(), std::io::Error> {
-        self.server_address = Some(address);
-
-        match TcpStream::connect(address) {
-            Ok(stream) => {
-                self.tcp_stream = Some(stream);
-
+    pub fn connect(&mut self, address: String) -> Result<(), std::io::Error> {
+        self.server_address = address;
+        match TcpStream::connect(&self.server_address) {
+            Ok(mut stream) => {
                 let connect_packet_bytes = ConnectPacket::new(self.connect_flags).encode();
-                match self
-                    .tcp_stream
-                    .as_ref()
-                    .unwrap()
-                    .write_all(&connect_packet_bytes)
+                match stream.write_all(&connect_packet_bytes)
                 {
                     Ok(_) => {
-                        let my_stream = self.tcp_stream.as_ref().unwrap().try_clone().unwrap();
-                        let mut reader = io::BufReader::new(my_stream);
-                        let _handle = thread::spawn(move || {
-                            let received: Vec<u8> = reader.fill_buf().unwrap().to_vec();
-                            if received.len() > 0 {
-                                let packet_type: u8 = (received[0] & 0xf0) >> 4;
+                        let mut reader = io::BufReader::new(&stream);
+                        let received: Vec<u8> = reader.fill_buf().unwrap().to_vec();
+                        if received.len() > 0 {
+                            let packet_type: u8 = (received[0] & 0xf0) >> 4;
                                 match packet_type.into() {
                                     ControlPacketType::ConnAck => {
-                                        println!("Received a ConnAck");
-
                                         let conn_ack_packet = ConnAck::from_bytes(&received);
-                                        println!("{:?}", conn_ack_packet);
+                                        println!("Received a ConnAck packet: {:?}", conn_ack_packet);
                                     }
                                     ControlPacketType::Connect => todo!(),
                                     ControlPacketType::Publish => todo!(),
@@ -71,10 +60,39 @@ impl<'a> MyQuteKittyClient<'a> {
                                     ControlPacketType::Disconnect => todo!(),
                                     _ => println!("Received an unknown control packet!"),
                                 }
-                            }
-                            println!("Read {:?}", received);
-                            reader.consume(received.len());
-                        });
+                        }
+                        self.tcp_stream = Some(stream);
+
+                        // let _handle = thread::spawn(move || {
+                        //     let received: Vec<u8> = reader.fill_buf().unwrap().to_vec();
+                        //     if received.len() > 0 {
+                        //         let packet_type: u8 = (received[0] & 0xf0) >> 4;
+                        //         match packet_type.into() {
+                        //             ControlPacketType::ConnAck => {
+                        //                 println!("Received a ConnAck");
+
+                        //                 let conn_ack_packet = ConnAck::from_bytes(&received);
+                        //                 println!("{:?}", conn_ack_packet);
+                        //             }
+                        //             ControlPacketType::Connect => todo!(),
+                        //             ControlPacketType::Publish => todo!(),
+                        //             ControlPacketType::PubAck => println!("Received a PubAck"),
+                        //             ControlPacketType::PubRec => todo!(),
+                        //             ControlPacketType::PubRel => todo!(),
+                        //             ControlPacketType::PubComp => todo!(),
+                        //             ControlPacketType::Subscribe => todo!(),
+                        //             ControlPacketType::SubAck => println!("Received a SubAck"),
+                        //             ControlPacketType::Unsubscribe => todo!(),
+                        //             ControlPacketType::UnsubAck => println!("Received an UnsubAck"),
+                        //             ControlPacketType::PingReq => println!("Received a PingReq"),
+                        //             ControlPacketType::PingResp => println!("Received a PingResp"),
+                        //             ControlPacketType::Disconnect => todo!(),
+                        //             _ => println!("Received an unknown control packet!"),
+                        //         }
+                        //     }
+                        //     println!("Read {:?}", received);
+                        //     reader.consume(received.len());
+                        // });
 
                         return Ok(());
                     }
@@ -133,9 +151,9 @@ async fn main() {
     let connect_flags = 0b00000010; // clean session bit to 1, the rest to 0
     let mut mqtt_client = MyQuteKittyClient::new(connect_flags);
     let server_address = String::from("127.0.0.1:1883");
-    match mqtt_client.connect(&server_address) {
+    match mqtt_client.connect(server_address) {
         Ok(_) => {
-            //println!("Connected to {:?}", server_address);
+            println!("Connected to {:?}", mqtt_client.server_address);
             //thread::sleep(time::Duration::from_secs(1));
         }
         Err(error) => {
